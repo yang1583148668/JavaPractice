d60b7afef81f1a39d079db65341affc11647b706
yang1583148668 <1583148668@qq.com>
2021-02-20 22:44:03 +0800

    Day20test01

--- src/Day20/test01/Test01.java
+++ src/Day20/test01/Test01.java
@@ -1,5 +1,24 @@
 package Day20.test01;
 
-public class Test01 {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.junit.Test;
+
+
+
+
+
+
+
+
+public class Test01 {
+	@SuppressWarnings("all")
+	@Test
+	public void main1() {
+		List list = new ArrayList();
+		list.add("张三");
+		list.add(0, "李四");
+		System.out.println(list);
+	}
 }
